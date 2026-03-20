b0f48e37e41cd57b2e257470310bb28e5bd54c98
김성운 <47772096+swkim777@users.noreply.github.com>
2026-03-21 07:26:06 +0900

    Add files via upload

---
+++ TodoWebApp/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
